06b3a7e7f1db6f2bb599d7ac3d19adb2d1340ad4
Francisco Hanna <franciscohanna92@gmail.com>
2016-09-02 11:17:18 -0300

    Initial commit

---
+++ Cliente.js
@@ -0,0 +1,45 @@
+const net = require('net');
+const IP = '127.0.0.1';
+const PORT = 1337;
+
+var stdin = process.stdin;
+var stdout = process.stdout;
+
+// Handler for incoming data event in client socket
+var client = new net.Socket();
+
+// Opens the connection for socket IP:PORT
+client.connect(PORT, IP, function() {
+	console.log('*****Connected to server*****');
+
+	// First message after connection
+	stdout.write('Message to send: ');
+});
+
+// Handler for incoming data event in client socket
+client.on('data', function(data) {
+	console.log('Echo from server: ' + data);
+	stdout.write('\r\rMessage to send: ');
+});
+
+// Handler for close event in client socket
+client.on('close', function() {
+	client.end();
+	process.exit();
+});
+
+// Handler for error event in client socket
+client.on('error', function(err) {
+	if(err.code == 'ECONNRESET'){
+		console.log('\n\nConnection closed abruptly.');
+		console.log('Disconnected from server.');
+	}
+	else{
+		console.log(err);
+	}
+});
+
+// Handler for incoming data on the standar input
+stdin.on('data', function (chunk) {
+	client.write(chunk)	
+});
